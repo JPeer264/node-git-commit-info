5d6e2f0488fe64ac66a42381deb3da155798626d
JPeer264 <jan.oster94@gmail.com>
2020-07-12 09:19:55 +0200

    Feat: change to typescript

---
+++ __tests__/test.spec.ts
@@ -0,0 +1,85 @@
+import fs from 'fs-extra';
+import { homedir } from 'os';
+import path from 'path';
+import tempDir from 'temp-dir';
+import { v4 as uuidv4 } from 'uuid';
+
+import gitCommitInfo from '../index';
+
+const fixtures = path.join(tempDir, 'sgc', uuidv4());
+const localFixtures = path.join(process.cwd(), '__tests__', 'fixtures');
+
+const folders = [
+  'upToDate',
+  'multiline',
+  'merge',
+];
+
+beforeAll(() => {
+  fs.copySync(localFixtures, fixtures);
+  folders.map((folder) => fs.renameSync(path.join(fixtures, folder, 'git'), path.join(fixtures, folder, '.git')));
+});
+
+afterAll(() => {
+  folders.map((folder) => fs.renameSync(path.join(fixtures, folder, '.git'), path.join(fixtures, folder, 'git')));
+});
+
+test('up to date', () => {
+  const latestInfo = gitCommitInfo({ cwd: path.join(fixtures, 'multiline') });
+
+  expect(latestInfo.message).toBe('do not merge');
+  expect(latestInfo.author).toBe('JPeer264');
+  expect(latestInfo.email).toBe('jan.oster94@gmail.com');
+  expect(latestInfo.commit).toBe('19131a4a9465f38a5cab030beb173edd6e23c6de');
+  expect(latestInfo.shortCommit).toBe('19131a4');
+});
+
+test('specific commit', () => {
+  const latestInfo = gitCommitInfo({
+    cwd: path.join(fixtures, 'multiline'),
+    commit: '66d6043fb740278dac391ad8b41df74ef9e68afc',
+  });
+
+  expect(latestInfo.message).toBe('Add: index.js\n    \n    Here is more information in the body\n    \n    BREAKING CHANGE: yes, here is a footer');
+  expect(latestInfo.author).toBe('JPeer264');
+  expect(latestInfo.email).toBe('jan.oster94@gmail.com');
+  expect(latestInfo.commit).toBe('66d6043fb740278dac391ad8b41df74ef9e68afc');
+  expect(latestInfo.shortCommit).toBe('66d6043');
+});
+
+test('unknown commit hash', () => {
+  const latestInfo = gitCommitInfo({
+    cwd: path.join(fixtures, 'multiline'),
+    commit: 'does not work',
+  });
+
+  expect(!!latestInfo.error).toBe(true);
+});
+
+test('merge conflict - named automatically', () => {
+  const mergeInfo = gitCommitInfo({
+    cwd: path.join(fixtures, 'merge'),
+    commit: '76d090566587fa5e97035b8c133866eb0116d7c0',
+  });
+
+  expect(mergeInfo.commit).toBe('76d090566587fa5e97035b8c133866eb0116d7c0');
+  expect(mergeInfo.message).toBe('Merge branch \'test/merge\'');
+});
+
+test('merge conflict - named randomly', () => {
+  const mergeInfo = gitCommitInfo({
+    cwd: path.join(fixtures, 'merge'),
+    commit: 'e49bfdc2285f13aa5cc206a02a4f41b335026ea5',
+  });
+
+  expect(mergeInfo.commit).toBe('e49bfdc2285f13aa5cc206a02a4f41b335026ea5');
+  expect(mergeInfo.message).toBe('My message');
+});
+
+test('no git repo', () => {
+  const latestInfo = gitCommitInfo({
+    cwd: homedir(),
+  });
+
+  expect(latestInfo).toEqual({});
+});
--- index.ts
+++ index.ts
@@ -1,43 +1,41 @@
 import execa from 'execa';
 import isGit from 'is-git-repository';
-import { platform } from 'os';
 import path from 'path';
-import pathIsAbsolute from 'path-is-absolute';
+import isAbsolute from 'is-absolute';
+
+export interface GitCommitInfoOptions {
+  cwd?: string;
+  commit?: string;
+}
 
 const processCwd = process.cwd();
 const regex = /\s+([\s\S]*)/g; // matches everything after the first whitespace
 
-const gitCommitInfo = ({ cwd, commit } = {}) => {
+const gitCommitInfo = ({ cwd, commit }: GitCommitInfoOptions = {}) => {
   const thisCommit = commit || '';
 
   let thisPath = cwd || processCwd;
 
-  thisPath = pathIsAbsolute(thisPath) ? thisPath : path.join(cwd, thisPath);
+  thisPath = isAbsolute(thisPath) ? thisPath : path.join(cwd, thisPath);
 
   if (!isGit(thisPath)) {
     return {};
   }
 
   try {
-    let exec;
+    const { stdout } = execa.commandSync(`git --no-pager show ${thisCommit} --summary`, { cwd });
 
-    if (platform() === 'win32') {
-      exec = execa.shellSync(`pushd ${thisPath} & git --no-pager show ${thisCommit} --summary`);
-    } else {
-      exec = execa.shellSync(`(cd ${thisPath} ; git --no-pager show ${thisCommit} --summary)`);
-    }
-
-    const info = exec.stdout
+    const info = stdout
       .split('\n')
-      .filter(entry => entry.length !== 0);
+      .filter((entry) => entry.length !== 0);
     const mergeIndex = info[1].indexOf('Merge') === -1 ? 0 : 1;
 
-    const hash = new RegExp(regex).exec(info[0])[1];
+    const hash = (new RegExp(regex).exec(info[0]) || [])[1];
     const shortHash = hash.slice(0, 7);
     const author = new RegExp(regex).exec(info[1 + mergeIndex])[1].match(/([^<]+)/)[1].trim();
     const email = new RegExp(regex).exec(info[1 + mergeIndex])[1].match(/<([^>]+)>/)[1];
     const date = new RegExp(regex).exec(info[2 + mergeIndex])[1];
-    const message = exec.stdout.split('\n\n')[1].trim();
+    const message = stdout.split('\n\n')[1].trim();
 
     return {
       hash,
